826d71ccae3e0110b3d880ad389cdf51570ad09f
Tobias Björch <t.bjorch@gmail.com>
2020-09-17 21:47:10 +0200

    Refactoring

--- backend/shellscripts/run-tests.sh
+++ backend/shellscripts/run-tests.sh
@@ -1,5 +1,5 @@
 rm -rf .pytest_cache
-export PYTHONPATH="/Users/tobj/Devprojects/blog_oo/"
+export PYTHONPATH="/Users/tobj/Devprojects/flask-react-blog/"
 export ENVIRONMENT="dev"
 echo "Running pytest..."
 pytest -v
